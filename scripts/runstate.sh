026d3bebbc28f66d69295d2a5e0fe662c460b4bc
twopoint71 <twopoint71@gmail.com>
2019-08-30 13:15:38 -0700

    Added help to runstate script

--- scripts/runstate.sh
+++ scripts/runstate.sh
@@ -2,7 +2,15 @@
 
 if [ -z ${1} ]
   then
-    echo "Please supply a state name"
+    STATES=$(/bin/ls -F1 /home/${USER}/salt-wad/salt/states | awk '{gsub(/.sls/, "", $0); printf("    %s\n", $0)}')
+    FIRST_STATE=$(/bin/ls -F1 /home/${USER}/salt-wad/salt/states | awk 'END {gsub(/.sls/, "", $0); printf("%s", $0)}')
+cat <<HELP
+Please supply a state name
+Valid state names are:
+${STATES}
+Example:
+    ${0} ${FIRST_STATE}
+HELP
     exit 1
   fi
 
